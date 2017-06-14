084cf7fc92a9d6f80a333bd6b8f6400a6559dc91
Adina Matei <potra.adina@gmail.com>
2017-06-14 16:53:46 +0300

    delete comments in app.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# starter-project
-Starter for projects - Node, Express
--- app.js
+++ app.js
@@ -3,18 +3,15 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-// __dirname - the path to the directory to bind to
 app.use(express.static(__dirname + '/public'));
 
-// get request
-// '/...' - the route = URL pattern + HTTP verb (text.txt, .ajax, parameters)
+
 app.get('/', function(request, response){
 	response.send('Hello World');
 	// response.type('json');
 	// response.end(JSON.stringify({keyName: name}));
 });
 
-//post method
 app.post('/', function(request, response){
 
 });
